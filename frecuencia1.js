fdbcf5ba65c9712e5a367f24d8129d4c3b7edfa5
carlosgj94 <carlosgj94@gmail.com>
2013-12-09 21:01:34 +0000

    Frecuencia detecta mayus y minus y test incluido. y tambien los nodos de cada caracter

--- frecuencia1.js
+++ frecuencia1.js
@@ -1,33 +1,3 @@
-function busqueda(texto){
-	var array = [];
-  
-	for(var i=0; i<texto.length; i++){
-		var pertenece = letra_incluida(texto[i], array);
-		if (pertenece === false){
-          var long = array.length;
-            array[long]=new Array(2);
-			array[long][0]= texto[i];
-			array[long][1]= 1;
-		}
-		else{
-			for (var j=0; j<array.length; j++){
-				if (texto[i]== array[j][0])
-					array[j][1]+=1;
-                    j=array.length;
-			}
-		}
-	}
-	return array;
-}
-
-function letra_incluida (letra, array) {
-	var incluida = false;
-	for (var i=0; i<array.length; i++){
-		if (letra == array[i][0])
-			incluida = true;
-	}
-	return incluida;
-}
 
 var texto = "ABCABCaB";
 console.log(busqueda(texto)); 
