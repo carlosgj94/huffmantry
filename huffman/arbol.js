d05f5d018bb63aed8cf0de55000537691e683afb
Carlos <carlosgj94@gmail.com>
2014-02-03 18:20:45 +0100

    Creador de arboles hecho

---
+++ huffman/arbol.js
@@ -0,0 +1,96 @@
+function crearArbol() {
+    //Creamos el arbol
+    var posX = 450;
+    var posY = 50;
+    
+    var parrafo = $("#texto").val();
+    var buscado = busqueda(parrafo);
+    var peso = imprimirPesos(buscado);
+    var nodos_creados = crear_nodos(buscado);
+    var arbol = crear_arbol(nodos_creados);
+    letra= (arbol[0].carac);
+    //dibujarNodo(posX, posY, letra);
+    recorrer(arbol[0], posX, posY, 0,5);
+}
+
+function dibujarNodo(posX, posY, letra) {
+    var c = document.getElementById("tutorial");
+    var cxt = c.getContext("2d");
+    cxt.fillStyle = "#1abc9c";
+    cxt.beginPath();
+    cxt.arc(posX, posY, 20, 0, Math.PI * 2, true);
+    cxt.closePath();
+    cxt.fill();
+
+    var ctx2 = c.getContext("2d");
+    cxt.fillStyle = "#333";
+    ctx2.font = "bold 20px sans-serif";
+    ctx2.fillText(letra, posX - 10, posY + 5);
+}
+function dibujarLinea(posXI, posYI, posXF, posYF){
+    var c = document.getElementById("tutorial");
+    var lienzo = c.getContext("2d");
+    lienzo.strokeStyle = "#874F37";
+    //Inicio de camino
+    lienzo.beginPath();
+    lienzo.moveTo(posXI,posYI);
+    lienzo.lineTo(posXF,posYF);
+    //Trazar linea
+    lienzo.stroke();
+}
+function recorrer(arbol, posX, posY, veces){
+    if( arbol != null){
+        dibujarNodo(posX, posY, arbol.carac );
+        veces++;
+       var existe = recorrer( arbol.izq, posX-(210/veces), posY+100, veces);
+        if (existe != false){
+            dibujarLinea(posX, posY, posX-(210/veces), posY+100);   
+        }
+        recorrer( arbol.der, posX+(210/veces), posY+100, veces);
+        if (existe != false){
+            dibujarLinea(posX, posY, posX+(210/veces), posY+100);   
+        }
+        return true;
+    }
+    return false;
+}
+/*
+      //Segundo
+      posX=posX+100;
+      cxt.fillStyle ="#1abc9c";
+        cxt.beginPath();
+        cxt.arc(posX,posY,30,0,Math.PI*2,true);
+      	cxt.closePath();
+        cxt.fill();
+      
+      var ctx2 =c.getContext("2d");
+      cxt.fillStyle ="#333";
+      ctx2.font = "bold 20px sans-serif";
+      	ctx2.fillText("B",posX-10,posY+5);
+
+      //Tercero
+      posX=posX+100;
+       cxt.fillStyle ="#1abc9c";
+        cxt.beginPath();
+        cxt.arc(posX,posY,30,0,Math.PI*2,true);
+      	cxt.closePath();
+        cxt.fill();
+      
+      var ctx2 =c.getContext("2d");
+      cxt.fillStyle ="#333";
+      ctx2.font = "bold 20px sans-serif";
+      	ctx2.fillText("C",posX-10,posY+5);
+      
+      //Avance en sentido Y
+       posY=posY+100;
+       posX=posX-50;
+       cxt.fillStyle ="#1abc9c";
+        cxt.beginPath();
+        cxt.arc(posX,posY,30,0,Math.PI*2,true);
+      	cxt.closePath();
+        cxt.fill();
+      
+      var ctx2 =c.getContext("2d");
+      cxt.fillStyle ="#333";
+      ctx2.font = "bold 20px sans-serif";
+      	ctx2.fillText("BC",posX-10,posY+5);*/
